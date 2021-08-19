d796f3027aaf90cb5e84fb2e9507791e5c2e0d77
AnhVanGiang <vangianganh@gmail.com>
2021-08-19 12:27:13 +0200

    add main codes

---
+++ src/extra.py
@@ -0,0 +1,207 @@
+import pygame
+from pygame import Rect, Color
+import random
+
+colors = {
+    "WHITE": (255, 255, 255),
+    "RED": (255, 0, 0),
+    "NAVYBLUE": (60, 60, 100),
+    "DARKBLUE": (0, 0, 139),
+    "YELLOW": (255, 255, 0),
+    "BLACK": (0, 0, 0)
+}
+
+directions = [
+    (0, -1),
+    (0, 1),
+    (1, 0),
+    (-1, 0)
+]
+
+
+class NRect(Rect):
+    """
+    New rectangle object inheritted from pygame.Rect
+    """
+
+    def __init__(self,
+                 x: int, y: int, width: int, height: int,
+                 color: str = "WHITE"):
+        super(NRect, self).__init__(x, y, width, height)
+        self._color = color
+
+    def get_color(self) -> tuple[int, int, int]:
+        """
+        Get color of the rectangle as a RGB tuple
+        :return:
+        """
+        return colors[self._color]
+
+    def set_color(self, color: str) -> None:
+        """
+        Set color
+        :param color:
+        :return:
+        """
+        self._color = color
+
+    @property
+    def color_str(self) -> str:
+        """
+        Get color of the rectangle as string
+        :return:
+        """
+        return self._color
+
+
+class Board:
+    def __init__(self, size: int, gap_size: int, wind_width: int):
+        self._boards = []
+        self._gap = gap_size
+        self._wwidth = wind_width
+        self._size = size
+        self._bsize = self._wwidth // self._size + self._gap
+
+    def change_cell_col(self, x: int, y: int, color: str) -> None:
+        self._boards[x][y].set_color(color)
+
+    def get_col(self, x: int, y: int) -> str:
+        return self._boards[x][y].color_str
+
+    def box_coords(self, x: int, y: int) -> tuple[int, int]:
+        """
+        Convert cell's coordinate on board to screen coordinate
+        :param x: row
+        :param y: column
+        :return:
+        """
+        x = x * (self._bsize + self._gap)
+        y = y * (self._bsize + self._gap)
+        return x, y
+
+    @property
+    def size(self) -> int:
+        """
+        Get size of the board
+        :return:
+        """
+        return self._size
+
+    def create_board(self) -> None:
+        """
+        Create the whole board
+        :return:
+        """
+        for i in range(self._size):
+            self._boards.append([])
+            for j in range(self._size):
+                x, y = self.box_coords(i, j)
+                rect = NRect(x, y, self._bsize, self._bsize)
+                self._boards[i].append(rect)
+
+    def get_rect(self, x: int, y: int) -> NRect:
+        """
+        Get the NRect object at a board coordinate
+        :param x:
+        :param y:
+        :return:
+        """
+        return self._boards[x][y]
+
+    def set_rect(self, x: int, y: int, rect: NRect) -> None:
+        """
+        Change the NRect object at a coordinate
+        :param x:
+        :param y:
+        :param rect:
+        :return:
+        """
+        self._boards[x][y] = rect
+
+    def get_board1(self) -> list[list[NRect]]:
+        """
+        Get the whole board as 2d array
+        :return:
+        """
+        return self._boards
+
+    def get_board2(self) -> list[NRect]:
+        """
+        Get the whole board as 1d array
+        :return:
+        """
+        b = []
+        for lst in self._boards:
+            b.extend(lst)
+        return b
+
+    def reset_color(self) -> None:
+        """
+        Change the colors of every cell to WHITE
+        :return:
+        """
+        board = self.get_board2()
+        for rect in board:
+            rect.set_color("WHITE")
+
+    def count_dist(self, color: str) -> int:
+        """
+        Count the number of cells of a color
+        :param color:
+        :return:
+        """
+        c = 0
+        board = self.get_board2()
+        for rect in board:
+            if rect.color_str == color:
+                c += 1
+        return c
+
+    def get_neighbors(self, x: int, y: int, walls: False) -> list[tuple[int, int]]:
+        """
+        Get the four directional neighbors of a cell. If walls is True, only get the ones where they arent black
+        :param x:
+        :param y:
+        :param walls:
+        :return:
+        """
+        ret = []
+        for i in directions:
+            nx, ny = x + i[0], y + i[1]
+            if not walls:
+                if (0 <= nx < self._size - 1) and (0 <= ny < self._size - 1):
+                    ret.append((nx, ny))
+            else:
+                board = self.get_board1()
+                if (0 <= nx < self._size - 1) \
+                        and (0 <= ny < self._size - 1) \
+                        and board[nx][ny].color_str != "BLACK":
+                    ret.append((nx, ny))
+        return ret
+
+    def mark_rects(self, rects: list[tuple[int, int]]) -> None:
+        """
+        Paint the list of cells as a color
+        :param rects:
+        :return:
+        """
+        board = self.get_board1()
+        for i in rects:
+            board[i[0]][i[1]].set_color("YELLOW")
+
+    def random_cell(self, color: str = "WHITE", edge: bool = False) -> tuple[int, int]:
+        """
+        Get a random cell on the board
+        :return:
+        """
+        lst = []
+        board = self.get_board1()
+        if edge:
+            a = 1
+        else:
+            a = 0
+        for i in range(a, self.size - a):
+            for j in range(a, self.size - a):
+                if board[i][j].color_str() == color:
+                    lst.append((i, j))
+        return random.choice(lst)
---
+++ src/main.py
@@ -0,0 +1,152 @@
+import sys
+from pygame.locals import *
+from src.extra import *
+
+FPS = 60  # frames per second, the general speed of the program
+WINDOWWIDTH = 800  # size of window's width in pixels
+GAPSIZE = 1  # size of gap between boxes in pixels
+BOARDSIZE = 100
+
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWWIDTH))
+fps_clock = pygame.time.Clock()
+
+board = Board(BOARDSIZE, GAPSIZE, WINDOWWIDTH)
+
+board.create_board()
+
+moving = False
+cur_col = "RED"
+
+instruction = """
+
+a: continuous drawing with RED
+d: continuous drawing with BLUE
+b: continuous drawing with BLACK (wall)
+left click: draw 1 block with RED
+right click: draw 1 block with BLUE
+
+"""
+
+
+def check_collision(event: pygame.event,
+                    board: Board,
+                    color: str = "RED") -> None:
+    """
+    Check if mouse event touches any cells and change that cell color
+    :param event: pygame event
+    :param board: board to play on
+    :param color: color to draw on board
+    :return:
+    """
+    rects = board.get_board2()
+    for item in rects:
+        if item.collidepoint(event.pos):
+            item.set_color(color)
+
+
+def color_all(board: Board, color: str):
+    """
+    Color all cells on board
+    :param board: board to color
+    :param color:
+    :return:
+    """
+    board = board.get_board2()
+    for b in board:
+        b.set_color(color)
+
+
+def get_bcoord(event: pygame.event, board: Board) -> tuple[int, int]:
+    """
+    Get the board coordinate of a mouse event
+    :param event: pygame event
+    :param board:
+    :return:
+    """
+    rects = board.get_board1()
+    n = len(rects)
+    for i in range(n):
+        for j in range(n):
+            if rects[i][j].collidepoint(event.pos):
+                return i, j
+
+
+def draw_board(surface: pygame.display, board: Board) -> None:
+    """
+    Draw the whole board on a surface
+    :param surface: pygame display
+    :param board:
+    :return:
+    """
+    rects = board.get_board2()
+    for item in rects:
+        pygame.draw.rect(surface, item.get_color(), item)
+
+
+# def check_surrounding(x: int, y: int, board: Board) -> int:
+#     s = 0
+#     if board.get_col(x - 1, y) == "WHITE":
+#         s += 1
+#     elif board.get_col(x + 1, y) == "WHITE":
+#         s += 1
+#     elif board.get_col(x, y - 1) == "WHITE":
+#         s += 1
+#     elif board.get_col(x, y + 1) == "WHITE":
+#         s += 1
+#     return s
+
+
+# def create_maze(board):
+#     """
+#     Generate maze on board using randomized Prim algorithm
+#     :param board:
+#     :return:
+#     """
+#     color_all(board, "YELLOW")
+#     paths = [[0 for i in board.size] for j in board.size]
+#     sx, sy = board.random_cell("YELLOW", True)
+#     paths[sx][sy] = "C"
+#     board.change_cell_col(sx, sy, "WHITE")
+#     board.mark_rects(board.get_neighbors(sx, sy, True), "BLACK")
+#
+#     while board.count_dist("BLACK") > 0:
+#         rwx, rwy = board.random_cell("BLACK")
+#
+#         if 0 < rwy < board.size:
+#             if board.get_col(rwx, rwy - 1) == "YELLOW" and board.get_col(rwx, rwy + 1) == "WHITE":
+#                 if check_surrounding(rwx, rwy, board) < 2:
+#                     board[rwx][rwy].color_str = "WHITE"
+#             elif board.get_col(rwx, rwy - 1) == "WHITE" and board.get_col(rwx, rwy + 1) == "YELLOW":
+#                 pass
+#         if 0 < rwx < board.size:
+#             if board.get_col(rwx - 1, rwy) == "YELLOW" and board.get_col(rwx + 1, rwy) == "WHITE":
+#                 pass
+#             elif board.get_col(rwx - 1, rwy) == "WHITE" and board.get_col(rwx + 1, rwy) == "YELLOW":
+#                 pass
+
+
+while True:
+    DISPLAYSURF.fill(colors["NAVYBLUE"])
+    draw_board(DISPLAYSURF, board)
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == pygame.K_q):
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+            check_collision(event, board)
+        elif event.type == MOUSEBUTTONDOWN and event.button == 3:
+            check_collision(event, board, "DARKBLUE")
+        elif event.type == MOUSEMOTION and moving:
+            check_collision(event, board, cur_col)
+        elif event.type == KEYDOWN:
+            if event.key == pygame.K_a:
+                moving = not moving
+                cur_col = "RED"
+            elif event.key == pygame.K_d:
+                moving = not moving
+                cur_col = "DARKBLUE"
+            elif event.key == pygame.K_r:
+                board.reset_color()
+
+    pygame.display.update()
+    fps_clock.tick(FPS)
